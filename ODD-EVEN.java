c031ebbaaec2db5f0384d2d728b47c33857ecdf3
Yashkumar1160 <yk4442144@gmail.com>
2025-06-09 11:23:29 +0530

    ODDEVEN

---
+++ ODD-EVEN.java
@@ -0,0 +1,8 @@
+public class ODD-EVEN{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in());
+		int n =sc.nextInt();
+		if(n%2==0){System.out.print("EVEN");}
+		else{System.out.print("ODD");}
+	}
+}
--- test.java
+++ test.java
@@ -5,4 +5,4 @@ public class test{
 			System.out.println(arr[i]);
 		}
 	}
-}
+} 
